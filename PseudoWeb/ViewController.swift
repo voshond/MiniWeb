7b8d8c3520b5b0b3077070f3fe97846ee4bf0899
WillBishop <will.bishop@gihs.sa.edu.au>
2018-09-24 19:24:55 +0930

    Initial Commit

---
+++ PseudoWeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PseudoWeb
+//
+//  Created by Will Bishop on 24/9/18.
+//  Copyright © 2018 Will Bishop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
