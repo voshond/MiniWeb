480e59b1b00a6b276ca02cb584a055ba5b8c66f2
WillBishop <will.bishop@gihs.sa.edu.au>
2018-11-05 21:44:06 +1030

    Added basic input option

---
+++ PseudoWeb WatchKit Extension/inputController.swift
@@ -0,0 +1,40 @@
+//
+//  inputController.swift
+//  PseudoWeb WatchKit Extension
+//
+//  Created by Will Bishop on 5/11/18.
+//  Copyright © 2018 Will Bishop. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class inputController: WKInterfaceController {
+
+    @IBOutlet weak var inputButton: WKInterfaceButton!
+    var input: String? = nil
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    @IBAction func editInput() {
+        self.presentTextInputController(withSuggestions: ["chirpapp.io"], allowedInputMode: .plain, completion: {result in
+            guard let result = result else {return}
+            guard let first = result.first else {return}
+            guard let input = first as? String else {return}
+            self.inputButton.setTitle("www.\(input)")
+            self.input = "www.\(input)"
+        })
+    }
+
+    @IBAction func viewWebsite() {
+        guard let input = input else {return}
+        if let url = URL(string: "http://\(input)"){
+            self.pushController(withName: "linkViewer", context: url)
+        }
+    }
+   
+}
